09cb507a64045733d7ebd4f7308aec8e960125c3
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-05 14:06:18 +0200

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente il suo nome
+
+
+// Chiedi all'utente il suo cognome
+
+
+
+// Chiedi all'utente il suo colore preferito
+
+
+// Scrivi sulla pagina nomecognomecolorepreferito22
