692a1797edbccf5e0dc23041b23a79536781d0b8
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-05 15:35:07 +0200

    add password without withespace

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
 // Chiedi all’utente il suo nome
-const firstName = prompt('Qual è il tuo nome?')
+const firstName = prompt('Qual è il tuo nome?').trim();
 
 // Chiedi all'utente il suo cognome
-const lastName = prompt('Qual è il tuo cognome?')
+const lastName = prompt('Qual è il tuo cognome?').trim();
 
 // Chiedi all'utente il suo colore preferito
-const color = prompt ('Qual è il tuo colore preferito?')
+const color = prompt ('Qual è il tuo colore preferito?').trim();
 
 /* console.log(`${firstName}${lastName}${color}`); */
 
 // Scrivi sulla pagina nomecognomecolorepreferito22
-document.getElementById('password').innerHTML = `La tua password è:${firstName[0].toUpperCase() + firstName.slice(1)}${lastName[0].toUpperCase() + lastName.slice(1)}${color}22`
+document.getElementById('password').innerHTML = `La tua password è:${firstName[0].toUpperCase() + firstName.slice(1)}${lastName[0].toUpperCase() + lastName.slice(1)}${color}22`;
