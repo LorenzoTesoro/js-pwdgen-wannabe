e24ceb7d78e0b5832e6a7a49674a1e562df5cfce
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-05 14:14:07 +0200

    add testing variables

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,13 @@
 // Chiedi all’utente il suo nome
-
+const firstName = prompt('Qual è il tuo nome?')
 
 // Chiedi all'utente il suo cognome
-
+const lastName = prompt('Qual è il tuo cognome?')
 
 
 // Chiedi all'utente il suo colore preferito
+const color = prompt ('Qual è il tuo colore preferito?')
 
 
+console.log(`${firstName}${lastName}${color}`);
 // Scrivi sulla pagina nomecognomecolorepreferito22
