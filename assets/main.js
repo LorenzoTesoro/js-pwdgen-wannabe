83f14e997333b4c2d74f86f09422493ae6962aed
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-05 14:50:53 +0200

    add password generator

--- assets/main.js
+++ assets/main.js
@@ -4,10 +4,12 @@ const firstName = prompt('Qual è il tuo nome?')
 // Chiedi all'utente il suo cognome
 const lastName = prompt('Qual è il tuo cognome?')
 
-
 // Chiedi all'utente il suo colore preferito
 const color = prompt ('Qual è il tuo colore preferito?')
 
+/* console.log(`${firstName}${lastName}${color}`); */
 
-console.log(`${firstName}${lastName}${color}`);
+
 // Scrivi sulla pagina nomecognomecolorepreferito22
+
+document.getElementById('password').innerHTML = `La tua password è:${firstName}${lastName}${color}22`
